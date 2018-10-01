1cf79932bfb0957ac10329ef6f54414519d45bdf
david m flynn <flynndave1@msn.com>
2018-10-01 23:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file.
+const drivers = ["Milo" , "Otis"  , "Garfield"];
+
+let lowerCaseDrivers 
+
+// const object3 = {};
+  for (const key in drivers) {
+    console.log(drivers[key].toLowerCase());    
+      }
+  console.log( drivers);
+  // console.log( lowerCaseDrivers(drivers));
